9d4f327886b6c618661f9f2353932c943cd474ec
A.Nalivaiko <analivaiko@atlaz.io>
2017-05-03 12:38:38 +0300

    global index was changed

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export * from "./dist/index";
+import { ModuleWithProviders } from '@angular/core';
+export * from './dist/sample.component';
+export * from './dist/sample.directive';
+export * from './dist/sample.pipe';
+export * from './dist/sample.service';
+export declare class SampleModule {
+    static forRoot(): ModuleWithProviders;
+}
