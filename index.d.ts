690892f9ba122cae7debea599925924f18c0977b
A.Nalivaiko <analivaiko@atlaz.io>
2017-05-03 12:42:19 +0300

    index was changed

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './dist/sample.component';
-export * from './dist/sample.directive';
-export * from './dist/sample.pipe';
-export * from './dist/sample.service';
-export declare class SampleModule {
-    static forRoot(): ModuleWithProviders;
-}
